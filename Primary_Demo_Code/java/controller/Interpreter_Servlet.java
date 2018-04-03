39bee012fe5b7ce3f1cad00e9ffe1cc54c1ddf66
Andrew Cowden <am.cowden.97@gmail.com>
2018-04-02 22:05:00 -0500

    Updated Current Working Directory Manager and File Storage Locations

--- Primary_Demo_Code/java/controller/Interpreter_Servlet.java
+++ Primary_Demo_Code/java/controller/Interpreter_Servlet.java
@@ -12,27 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Interpreter_Servlet extends HttpServlet {
 
-
     /**
      * Processes requests for the HTTP POST method.
      *
      * @param request servlet request
      * @param response servlet response
+     * @param recievedRCode
      * @param interpretedRCode interpreted R Code from Model
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response,
-            String interpretedRCode) throws ServletException, IOException {
-        
+            String recievedRCode, String interpretedRCode) throws ServletException, IOException {
+
         //Create New JSP Page To Display Interpreted R Code
         response.setContentType("text/html;charset=UTF-8");
+        request.setAttribute("r_input", recievedRCode);
         request.setAttribute("r_output", interpretedRCode);
         RequestDispatcher jspDispatcher = request.getRequestDispatcher("/Interpreter_View.jsp");
         jspDispatcher.forward(request, response);
     }
 
-        
     /**
      * Handles the HTTP POST method.
      *
@@ -44,16 +44,18 @@ public class Interpreter_Servlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         try {
             //Get User's R Code and Send Out to Interpreter
+            String completedRCode, recievedRCodeFilename;
             String recievedRCode = request.getParameter("r_input");
-            String completedCode;
             R_Driver interpreter = new model.R_Driver();
-           
-            completedCode = interpreter.interpretCode(recievedRCode); //Send Code To Application Server Side
-            processRequest(request, response, completedCode); //Send Interpreted Code to User
-            
+            CWD_Manager fileMaker = new CWD_Manager();
+
+            recievedRCodeFilename = fileMaker.writeRToFile(recievedRCode);        //Save R Input as File in CWD
+            completedRCode = interpreter.interpretCode(recievedRCodeFilename);    //Send R File To Application Server Side
+            processRequest(request, response, recievedRCode, completedRCode);     //Send Interpreted Code to User
+
         } catch (InterruptedException ex) {
             Logger.getLogger(Interpreter_Servlet.class.getName()).log(Level.SEVERE, null, ex);
         }
