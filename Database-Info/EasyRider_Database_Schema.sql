d7c0f254d9694231ca7a0a60af8dece6687fa88f
Andrew Cowden <am.cowden.97@gmail.com>
2018-04-04 22:45:06 -0500

    Updated Syntax and Removed Conditionals for Schema

--- Database-Info/EasyRider_Database_Schema.sql
+++ Database-Info/EasyRider_Database_Schema.sql
@@ -1,19 +1,17 @@
-CREATE TABLE Account_Info(
+CREATE TABLE IF NOT EXISTS Account_Info (
 	username VARCHAR(255) PRIMARY KEY,
-	password VARCHAR(255) NOT NULL,
+	hashedword VARCHAR(255) NOT NULL,
 	firstname VARCHAR(255) NOT NULL,
 	lastname VARCHAR(255) NOT NULL
-	CONSTRAINT CHK_UserLength CHECK (CHAR_LENGTH(username) >= 4),
-	CONSTRAINT CHK_UserPassword CHECK (CHAR_LENGTH(password) >= 6)
 );
 
 
-CREATE TABLE R_File(
-	username VARCHAR(255),
+CREATE TABLE IF NOT EXISTS R_File (
+	file_owner VARCHAR(255),
 	filename VARCHAR(255),
 	data_file MEDIUMBLOB NOT NULL,
-	PRIMARY KEY(username, filename)
-	FOREIGN KEY(username) REFERENCES Account_Info(username)
+	PRIMARY KEY(file_owner, filename),
+	FOREIGN KEY(file_owner) REFERENCES Account_Info(username)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 );
