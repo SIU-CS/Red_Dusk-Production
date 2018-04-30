c6f0572d13f92758047e0aae113acc9668f10c19
Andrew Cowden <am.cowden.97@gmail.com>
2018-04-30 17:31:40 -0500

    Updated Database Schema to Include Text rather than Blob sql type

--- Installation_Files/EasyRider_Database_Schema.sql
+++ Installation_Files/EasyRider_Database_Schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Account_Info (
 	username VARCHAR(255) PRIMARY KEY,
-	hashedword VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
 	firstname VARCHAR(255) NOT NULL,
 	lastname VARCHAR(255) NOT NULL
 );
@@ -8,9 +8,10 @@ CREATE TABLE IF NOT EXISTS Account_Info (
 
 CREATE TABLE IF NOT EXISTS R_File (
 	file_owner VARCHAR(255),
-	filename VARCHAR(255),
-	data_file MEDIUMBLOB NOT NULL,
-	PRIMARY KEY(file_owner, filename),
+	file_name VARCHAR(255),
+	last_modified DATETIME,
+	data_file MEDIUMTEXT NOT NULL,
+	PRIMARY KEY(file_owner, file_name),
 	FOREIGN KEY(file_owner) REFERENCES Account_Info(username)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
